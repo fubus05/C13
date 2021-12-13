3ea150c949356e6c3750e27ea498bc1c1918da55
wine-blip <savitskiy.i.y@gmail.com>
2021-12-14 00:06:04 +0800

    1
    1

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+public class Program
+{
+    public static void Main ()
+    {   
+        Console.WriteLine("Введите значение x");
+        double X = double.Parse(Console.ReadLine());
+        Console.WriteLine("Введите значение a");
+        double A = double.Parse(Console.ReadLine());
+
+        double calendar = Math.Log((A + Math.Abs(X)), Math.Pow(X, 2));
+        double denominator = Math.Sqrt(X - Math.Asin(X)/Math.Pow(Math.Acos(X + Math.Pow(A, 2)),5));
+
+        Console.WriteLine("y = " + calendar/denominator);
+    }
+}
